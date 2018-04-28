409cc9c18134b67b060a6fa6a6b5a58057576aa5
Echo009 <ech0.extreme@foxmail.com>
2018-04-29 00:06:40 +0800

    login and register page , nav bar

---
+++ js/common.js
@@ -0,0 +1,97 @@
+/**
+ * Author : Ech0
+ * Email  : ech0.extreme@foxmail.com
+ * Time   : 04/18/2018 08:59 AM
+ */
+// it depends on jQuery , sweetalert.js , axios.js
+function simpleSuccessInfo(content) {
+    swal({
+        icon:"success",
+        text:content,
+        button:false,
+        timer:MIDDLE_DELAY,
+    });
+    return true;
+}
+function simpleErrorInfo(content) {
+    swal({
+        icon:"error",
+        text:content,
+        button:false,
+        timer:MIDDLE_DELAY,
+    });
+    return true;
+}
+function simpleWarningInfo(content) {
+    swal({
+        icon:"warning",
+        text:content,
+        button:false,
+        timer:LONG_DELAY,
+    });
+    return true;
+}
+// xhr config
+var instance = axios.create({
+    baseURL: 'http://localhost/exam/',
+    timeout: 500,
+    withCredentials:true,
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+});
+function logout() {
+    instance.get('/common/logout')
+        .then(function (response) {
+            console.log(response);
+            simpleSuccessInfo("注销成功 ！")
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    store.remove(CURRENT_USER);
+    //refresh page
+    setTimeout("window.location.href='index.html'",MIDDLE_DELAY);
+}
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return false;
+}
+function updateUserInfo() {
+
+    instance.get('/common/userInfo')
+        .then(function (response) {
+            console.log("update userInfo :" + response.data.data.userName);
+            store.set(CURRENT_USER,response.data.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+
+
+
+$(function () {
+    $(document).on("click", ".search-option", function () {
+        var $this = $(this);
+        $("#searchOption").text($this.text());
+    });
+
+    $("[data-toggle='tooltip']").tooltip();
+    // todo modify
+    $("#search-btn").on("click", function () {
+        var category = $.trim($("#searchOption").text());
+        var keyword = $.trim($("#search-keyword").val());
+        var targetLink = "./search.html?bookName=" + keyword;
+        if (category != "所有分类") {
+            targetLink += "&category=" + category;
+        }
+        console.log(targetLink);
+        window.location.href = targetLink;
+    })
+});
---
+++ js/constant.js
@@ -0,0 +1,31 @@
+/**
+ * Author : Ech0
+ * Email  : ech0.extreme@foxmail.com
+ * Time   : 04/21/2018 12:33 AM
+ */
+
+
+const CURRENT_USER = "currentUser_exam";
+const USER_NAME = "userName_exam";
+const PASSWORD = "password_exam";
+const MICRO_DELAY = 100;
+const SHORT_DELAY = 500;
+const MIDDLE_DELAY = 700;
+const LONG_DELAY = 1000;
+const USER = 1;
+const ADMIN = 0;
+/**
+ SINGLE_SELECT(0,"单选"),
+ MULTI_SELECT(1,"多选"),
+ GAP_FILLING(2,"填空"),
+ ASK(3,"问答"),
+ CODE(4,"编程"),
+ */
+if(typeof QUESTION_TYPE == "undefined"){
+    var QUESTION_TYPE = {};
+    QUESTION_TYPE.SINGLE_SELECT = 0;
+    QUESTION_TYPE.MULTI_SELECT = 1;
+    QUESTION_TYPE.GAP_FILLING = 2;
+    QUESTION_TYPE.ASK = 3;
+    QUESTION_TYPE.CODE = 4;
+}
